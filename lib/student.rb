10f8a096f97ca8e596ec09a7c292631736b23e74
Monica  Karasek <mlkarasek@gmail.com>
2017-12-01 23:17:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
@@ -5,24 +6,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each{|key, value| self.send(("#{key}="), value)}
-  end
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
 
-@all << student 
-end
+  end
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student)
+    end
   end
-end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each{|key, value| self.send(("#{key}="), value)}
-    self
-end
-
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
 
   def self.all
     @@all
-    end
+  end
+end
